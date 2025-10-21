2161ff2af7534975a666fc32c560f28e605cb255
Gustavo Araujo <gustavoaraujopires9@gmail.com>
2025-10-21 17:18:11 -0300

    Usando Spring Boot

---
+++ src/main/java/com/produto/cadastro/Controller/ProdutoController.java
@@ -0,0 +1,42 @@
+package com.produto.cadastro.Controller;
+
+import com.produto.cadastro.Produtos.Produto;
+import com.produto.cadastro.Service.ProdutoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    private final ProdutoService Service;
+
+    public ProdutoController(ProdutoService service) {
+        Service = service;
+    }
+
+    @GetMapping
+    public List<Produto> carregaProduto(){
+        return Service.ListaDeProduto();
+    }
+
+    @PostMapping
+    public Produto salvarNovoProduto(@RequestBody Produto produto){
+        return Service.cadastarProduto(produto);
+    }
+
+    @GetMapping("/{id}")
+    public Produto buscarProdutoid(@PathVariable Long id){
+        return Service.BuscarProdutoId(id);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void deletarProdutos(@PathVariable Long id) {
+        Service.excluirProduto(id);
+    }
+
+
+}
---
+++ src/main/java/com/produto/cadastro/Produtos/Produto.java
@@ -0,0 +1,50 @@
+package com.produto.cadastro.Produtos;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String descricao;
+    private Double preco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
